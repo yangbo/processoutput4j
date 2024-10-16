0c7da3ff9864ed86d36d445d2d6795e25dad09ad
yangbo <bo.yang@telecwin.com>
2024-10-16 15:40:14 +0800

    add readLineSafe() to AbstractProcessReader, it will read line from m_Reader without hang on the caller.

--- src/main/java/com/github/fracpete/processoutput4j/reader/AbstractProcessReader.java
+++ src/main/java/com/github/fracpete/processoutput4j/reader/AbstractProcessReader.java
@@ -18,103 +18,130 @@ import java.io.InputStreamReader;
  * @author fracpete (fracpete at waikato dot ac dot nz)
  */
 public abstract class AbstractProcessReader
-  extends AbstractProcessRunnable {
+        extends AbstractProcessRunnable {
 
-  /** whether to use stdout or stderr. */
-  protected boolean m_Stdout;
+    /**
+     * whether to use stdout or stderr.
+     */
+    protected boolean m_Stdout;
 
-  /** the reader use internally. */
-  protected BufferedReader m_Reader;
+    /**
+     * the reader use internally.
+     */
+    protected BufferedReader m_Reader;
 
-  /**
-   * Initializes the reader.
-   *
-   * @param stdout	whether to read stdout or stderr
-   */
-  public AbstractProcessReader(boolean stdout) {
-    super();
-    m_Stdout = stdout;
-  }
-
-  /**
-   * Returns whether the reader is for stdout or stderr.
-   *
-   * @return		true if for stdout
-   */
-  public boolean isStdout() {
-    return m_Stdout;
-  }
-
-  /**
-   * For processing the line read from stdout/stderr.
-   *
-   * @param line	the output line
-   */
-  protected abstract void process(String line);
-
-  /**
-   * Flushes the data.
-   */
-  public void flush() {
-    String	line;
-
-    while (m_Reader != null) {
-      try {
-	line = m_Reader.readLine();
-	if (line == null)
-	  break;
-	else
-	  process(line);
-      }
-      catch (IOException e) {
-        // probably "stream closed"
-        break;
-      }
-      catch (Exception e) {
-        logError("Failed to flush!", e);
-	break;
-      }
+    /**
+     * Initializes the reader.
+     *
+     * @param stdout whether to read stdout or stderr
+     */
+    public AbstractProcessReader(boolean stdout) {
+        super();
+        m_Stdout = stdout;
     }
-  }
 
-  /**
-   * The actual processing loop.
-   */
-  protected void doRun() {
-    String 		line;
+    /**
+     * Returns whether the reader is for stdout or stderr.
+     *
+     * @return true if for stdout
+     */
+    public boolean isStdout() {
+        return m_Stdout;
+    }
 
-    try {
-      if (m_Stdout)
-	m_Reader = new BufferedReader(new InputStreamReader(m_Process.getInputStream()));
-      else
-	m_Reader = new BufferedReader(new InputStreamReader(m_Process.getErrorStream()));
+    /**
+     * For processing the line read from stdout/stderr.
+     *
+     * @param line the output line
+     */
+    protected abstract void process(String line);
 
-      while (m_Process.isAlive()) {
+    /**
+     * Flushes the data.
+     */
+    public void flush() {
+        String line;
+
+        while (m_Reader != null) {
+            try {
+                line = m_Reader.readLine();
+                if (line == null)
+                    break;
+                else
+                    process(line);
+            } catch (IOException e) {
+                // probably "stream closed"
+                break;
+            } catch (Exception e) {
+                logError("Failed to flush!", e);
+                break;
+            }
+        }
+    }
+
+    /**
+     * The actual processing loop.
+     */
+    protected void doRun() {
+        String line;
+
         try {
-          line = m_Reader.readLine();
+            if (m_Stdout)
+                m_Reader = new BufferedReader(new InputStreamReader(m_Process.getInputStream()));
+            else
+                m_Reader = new BufferedReader(new InputStreamReader(m_Process.getErrorStream()));
+
+            while (m_Process.isAlive()) {
+                try {
+                    // line = m_Reader.readLine();
+                    // To support rsync password input we can not use readLine(), because it will hang up the caller
+                    line = readLineSafe(1000);
+                } catch (IOException ioe) {
+                    // has process stopped?
+                    if (ioe.getMessage().toLowerCase().contains("stream closed")) {
+                        return;
+                    } else {
+                        throw ioe;
+                    }
+                }
+                if (line != null && !line.isEmpty()) {
+                    process(line);
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Failed to read from " + (m_Stdout ? "stdout" : "stderr") + " for process #" + m_Process.hashCode() + ":");
+            e.printStackTrace();
         }
-        catch (IOException ioe) {
-          // has process stopped?
-          if (ioe.getMessage().toLowerCase().contains("stream closed"))
-            return;
-          else
-            throw ioe;
+
+        try {
+            // make sure all data has been read
+            flush();
+        } catch (Exception e) {
+            // ignored
         }
-        if (line != null)
-          process(line);
-      }
-    }
-    catch (Exception e) {
-      System.err.println("Failed to read from " + (m_Stdout ? "stdout" : "stderr") + " for process #" + m_Process.hashCode() + ":");
-      e.printStackTrace();
     }
 
-    try {
-      // make sure all data has been read
-      flush();
+    /**
+     * Read line from m_Reader without hang on the caller.
+     *
+     * @param timeoutMs timeout in milliseconds.
+     *                  It is the time to wait and then return as a line even there is no Carriage Return characters.
+     *                  It is used for 'rsync password' scenarios.
+     * @return line read
+     */
+    private String readLineSafe(long timeoutMs) throws IOException {
+        StringBuilder line = new StringBuilder();
+        long startTime = System.currentTimeMillis();
+        while (System.currentTimeMillis() - startTime < timeoutMs) {
+            while (m_Reader.ready()) {
+                line.append((char) m_Reader.read());
+            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ignored) {
+            }
+        }
+
+        return line.toString();
     }
-    catch (Exception e) {
-      // ignored
-    }
-  }
 }
